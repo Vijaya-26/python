85118744b6c55feb7e03dfb9fdaea15d8995bb18
Vijaya-26 <83897655+Vijaya-26@users.noreply.github.com>
2021-08-28 13:04:48 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,2 +1,5 @@
->>> 5*'Python ''Bootcamp '
-'Python Bootcamp Python Bootcamp Python Bootcamp Python Bootcamp Python Bootcamp '
+a = input('enter 1st string: ')
+b = input('enter 2nd string: ')
+conc_str = a+b
+print(conc_str)
+print(5*conc_str)
