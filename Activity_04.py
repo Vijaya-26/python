577c6b2237b6deff5525a3e90488d1a1fd6c7470
Vijaya-26 <83897655+Vijaya-26@users.noreply.github.com>
2021-08-28 15:49:52 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -2,3 +2,22 @@ x=input()
 y=input()
 sum=int(x)+int(y)
 print("{}+{}={}".format(x,y,sum))
+
+
+x=input()
+y=input()
+sum=int(x)+int(y)
+print(x,"+",y,"=",sum)
+
+
+x=2
+y=3
+sum=a+b
+print("%d + %d=%d"%(x,y,sum))
+
+
+x=input()
+y=input()
+sum=int(x)+int(y)
+print(f'{x}+{y}={sum}')
+
