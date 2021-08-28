3cf269da518720a4852f502ebac26d4af054d153
Vijaya-26 <83897655+Vijaya-26@users.noreply.github.com>
2021-08-28 14:22:08 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,5 +1,5 @@
-a = input('enter 1st string: ')
-b = input('enter 2nd string: ')
+a = input('enter 1st string: \n')
+b = input('enter 2nd string: \n')
 conc_str = a+b
 print(conc_str)
 print(5*conc_str)
